a2a46f2e73b0ea83023b630ac768ee1cfd29a9b9
Tenundor <71014004+Tenundor@users.noreply.github.com>
2020-09-09 19:07:48 +0300

    Test

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 
 from settings import Settings
 import program_functions as pf
+from three_r_robot import ThreeRRobot
 
 
 def run_program():
@@ -10,9 +11,11 @@ def run_program():
     screen = pygame.display.set_mode((settings.screen_width, settings.screen_height))
     pygame.display.set_caption("3R inverse kinematic")
 
+    robot_left_conf = ThreeRRobot(screen, tau_list=[-1, 2, -1], length_list=[2, 1, 0.5])
+
     while True:
         pf.check_events()
-        pf.update_screen(settings,screen)
+        pf.update_screen(settings, screen, robot_left_conf)
 
 
 run_program()
