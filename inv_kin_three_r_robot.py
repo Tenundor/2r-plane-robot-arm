290deb662f75d67c6546d79148f4f7e74a11c76d
Tenundor <71014004+Tenundor@users.noreply.github.com>
2020-09-11 20:13:04 +0300

    Создал inv_kin_three_r_robot.py

---
+++ inv_kin_three_r_robot.py
@@ -0,0 +1,6 @@
+def inv_kin_3r_robot(end_ef_coord, end_ef_angle_degree):
+
+    robot_config = {'robot_config': {'left_robot_tau_list': [0, 0, 0], 'right_robot_tau_list': [0, 0, 0]},
+                    'work_space_out': False}
+
+    return robot_config
